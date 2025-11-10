3b91a17ec5346699cdae142791fa6cd450de6941
Jaqueline Paulino de Oliveira <jaqueline.oliveira@memora.com.br>
2025-11-10 14:57:28 -0300

    corrigindo codigo

--- backend/core/config.py
+++ backend/core/config.py
@@ -4,9 +4,6 @@ from dotenv import load_dotenv
 # Carrega o arquivo .env automaticamente
 load_dotenv()
 
-# Variáveis do SendGrid
-SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
-SENDGRID_FROM = os.getenv("SENDGRID_FROM", "no-reply@example.com")
 
 # Controle se o navegador roda visível ou oculto
 HEADLESS = os.getenv("HEADLESS", "true").lower() in ("1", "true", "yes")
